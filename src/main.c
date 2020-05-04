317dd66a081de300a0963b41f927b39298dae9f1
Demid Kashirin <demid.kashirin@gmail.com>
2020-05-04 17:36:08 +0300

    Implment `table_find` method instead of `table_end`

--- src/main.c
+++ src/main.c
@@ -203,6 +203,18 @@ Cursor* table_start(Table* table) {
   return cursor;
 }
 
+Cursor* table_find(Table* table, uint32_t key) {
+  uint32_t root_page_num = table->root_page_num;
+  void* root_node = get_page(table->pager, root_page_num);
+
+  if (get_node_type(root_node) == NODE_LEAF) {
+    return leaf_node_find(table, root_page_num, key);
+  } else {
+    printf("Need to implement searching an internal node\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
 void pager_flush(Pager* pager, uint32_t page_num) {
   if (pager->pages[page_num] == NULL) {
      printf("Tried to flush null page\n");
